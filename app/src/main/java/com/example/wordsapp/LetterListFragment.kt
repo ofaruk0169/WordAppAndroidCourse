aac6a8b7680644082dde40bc02662a4567eb73c4
Omare Faruk <ofaruk0169@gmail.com>
2023-11-07 21:38:08 +0000

    watched a Phillip Lackner video on Fragments and it explained it a lot better than the tutorials have from Android. I think I will tackle this again tomorrow with a fresh mind now that I get the gist of what a fragment is

---
+++ app/src/main/java/com/example/wordsapp/LetterListFragment.kt
@@ -0,0 +1,25 @@
+package com.example.wordsapp
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.wordsapp.databinding.FragmentLetterListBinding
+
+class LetterListFragment : Fragment() {
+    private var _binding: FragmentLetterListBinding? = null
+    private val binding get() = _binding!!
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+}
